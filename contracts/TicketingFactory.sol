3b6e01c2ccb2cced7eb9c29c5cd4c00b2ac01c5f
Teddy Not Bear <notbearteddy@gmail.com>
2022-11-15 00:06:20 +0100

    feat : create factory

--- contracts/TicketingFactory.sol
+++ contracts/TicketingFactory.sol
@@ -4,6 +4,75 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract TicketingFactory {
+import 'erc721a-upgradeable/contracts/ERC721AUpgradeable.sol';
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract TicketingFactory is 
+    ERC721AUpgradeable,
+    ReentrancyGuardUpgradeable,
+    OwnableUpgradeable ,
+    PausableUpgradeable
+{
+    using Counters for Counters.Counter;
+    Counters.Counter private _ticketIds;
+    using Strings for uint256;
+
+    string private baseURI;
+    uint256 maxTicketsSupply;
+
+    struct TicketingDetails {
+        uint256 id;
+        string name;
+        string symbol;
+        string description;
+        uint256 createdAt;
+        uint256 stopSellingTickets;
+    }
+
+    function initialize(
+        string memory _name,
+        string memory _symbol,
+        string memory _baseTokenURI,
+        uint256 _maxTicketsSupply
+    ) initializer public {
+        __ERC721A_init(_name, _symbol);
+        __ReentrancyGuard_init();
+        __Ownable_init();
+        __Pausable_init();
+
+        maxTicketsSupply = _maxTicketsSupply;
+        setBaseURI(_baseTokenURI);
+    }
+
+    function mintTickets() external payable returns (uint256) {
+        _ticketIds.increment();
+
+        uint256 newTicketId = _ticketIds.current();
+        _safeMint(msg.sender, newTicketId);
+
+        return newTicketId;
+    }
+
+    function _baseURI() internal view  virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    function setBaseURI(string memory _baseTokenURI) internal onlyOwner {
+        baseURI = _baseTokenURI;
+    }
+
+    function tokenURI(uint256 _ticketId) public view virtual override returns (string memory) {
+        require(_exists(_ticketId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory currentBaseURI = _baseURI();
+        return bytes(currentBaseURI).length > 0
+            ? string(abi.encodePacked(currentBaseURI, _ticketId.toString(),".json"))
+            : "";
+    }
 
 }
