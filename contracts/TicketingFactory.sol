c6477e4a354057cec2de78faaf4b6a550e2b2fcb
Teddy Not Bear <notbearteddy@gmail.com>
2022-11-27 11:53:41 +0100

    fix : remove struct

--- contracts/TicketingFactory.sol
+++ contracts/TicketingFactory.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
-import 'erc721a-upgradeable/contracts/ERC721AUpgradeable.sol';
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
@@ -13,59 +14,45 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract TicketingFactory is 
-    ERC721AUpgradeable,
+    ERC721URIStorageUpgradeable,
     ReentrancyGuardUpgradeable,
     OwnableUpgradeable ,
     PausableUpgradeable
 {
+    using Strings for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private _ticketIds;
-    Counters.Counter private _ticketingDetailsIds;
-    using Strings for uint256;
 
     string private baseURI;
-    uint256 maxTicketsSupply;
+    uint256 public maxTicketsSupply;
+    uint256 public endDateSale;
 
-    struct TicketingDetails {
-        string name;
-        string symbol;
-        string description;
-        uint256 createdAt;
-        uint256 stopSellingTickets;
-    }
-
-    mapping(uint256 => TicketingDetails) public ticketingDetails;
+    event MintedTicket(address indexed _owner, uint256 indexed _id);
 
     function initialize(
         string memory _name,
         string memory _symbol,
-        string memory _description,
         string memory _baseTokenURI,
         uint256 _maxTicketsSupply,
-        uint256 _stopSellingTickets
+        uint256 _endDateSale
     ) initializer public {
-        __ERC721A_init(_name, _symbol);
+        require(_endDateSale > block.timestamp + 1 days, 'Ticketing: the _endDateSale must be in more than 1 day');
+
+        __ERC721_init(_name, _symbol);
         __ReentrancyGuard_init();
         __Ownable_init();
         __Pausable_init();
-        uint256 ticketingDetailsId = _ticketingDetailsIds.current();
         maxTicketsSupply = _maxTicketsSupply;
-        ticketingDetails[ticketingDetailsId] = TicketingDetails({
-            name: _name,
-            symbol: _symbol,
-            description: _description,
-            createdAt: block.timestamp,
-            stopSellingTickets: _stopSellingTickets
-        });
+        endDateSale = _endDateSale;
         setBaseURI(_baseTokenURI);
-        _ticketingDetailsIds.increment();        
     }
 
-    function mintTickets() external payable returns (uint256) {
+    function mintTickets() public {
         _ticketIds.increment();
         uint256 newTicketId = _ticketIds.current();
         _safeMint(msg.sender, newTicketId);
-        return newTicketId;
+        tokenURI(newTicketId);
+        emit MintedTicket(msg.sender, newTicketId);
     }
 
     function _baseURI() internal view  virtual override returns (string memory) {
@@ -77,11 +64,10 @@ contract TicketingFactory is
     }
 
     function tokenURI(uint256 _ticketId) public view virtual override returns (string memory) {
-        require(_exists(_ticketId), "ERC721Metadata: URI query for nonexistent token");
+        require(_exists(_ticketId), "Metadata: URI query for nonexistent ticket");
         string memory currentBaseURI = _baseURI();
         return bytes(currentBaseURI).length > 0
             ? string(abi.encodePacked(currentBaseURI, _ticketId.toString(),".json"))
             : "";
     }
-
 }
