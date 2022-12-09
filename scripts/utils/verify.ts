93263cc151ff674bfb62db9fce0df59dc7659b6f
Teddy Not Bear <notbearteddy@gmail.com>
2022-12-09 17:22:55 +0100

    fix : reformat task name

--- scripts/utils/verify.ts
+++ scripts/utils/verify.ts
@@ -2,7 +2,7 @@ import { run } from "hardhat";
 
 const verify = async (contractAddress: string, args: any[]) => {
   try {
-    await run("verify:berify", {
+    await run("verify:verify", {
       address: contractAddress,
       constructorArguments: args
     });
